3845f4f2809a30feec48ca2f8d1e672bb2da1a49
mroldsma <81998465+mroldsma@users.noreply.github.com>
2022-03-11 14:40:42 -0300

    section hero finished, services almost, started section brands

---
+++ app.js
@@ -0,0 +1,46 @@
+const heroSpanSlide = document.querySelector('.p-slider').children;
+const spanLenght = heroSpanSlide.length;
+let index = 0;
+const spanInTimer = 3000;
+const spanOutTimer = 2800;
+
+
+
+function animateHeroSpan(){
+  for (let i = 0; i < spanLenght; i++) {
+    heroSpanSlide[i].classList.remove('span-in','span-out');
+  }
+
+  heroSpanSlide[index].classList.add('span-in');
+
+  setTimeout(() => {
+    heroSpanSlide[index].classList.add('span-out');
+  }, spanOutTimer);
+
+  setTimeout(() => {
+    if(index == spanLenght-1){
+      index = 0;
+    }
+    else{
+      index++;
+    }
+
+    animateHeroSpan();
+  }, spanInTimer);
+}
+
+
+
+
+
+window.onload = animateHeroSpan;
+
+
+
+
+
+
+
+
+
+console.log(spanLenght)
